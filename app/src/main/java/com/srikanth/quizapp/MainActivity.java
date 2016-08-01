f84b4a85b2170c673803f9904082b6e5204ddf37
SrikanthGovindan <srikanthgjun05@gmail.com>
2016-08-01 23:07:50 +0530

    Second Commit

--- app/src/main/java/com/srikanth/quizapp/MainActivity.java
+++ app/src/main/java/com/srikanth/quizapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -15,10 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.rGroup);
         RadioGroup radioGroupOne = (RadioGroup) findViewById(R.id.rGroup1);
         RadioGroup radioGroupTwo = (RadioGroup) findViewById(R.id.rGroup3);
         RadioGroup radioGroupThree = (RadioGroup) findViewById(R.id.rGroup4);
+
+
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.cBox1);
+        final CheckBox checkBox1 = (CheckBox) findViewById(R.id.cBox2);
+        final CheckBox checkBox2 = (CheckBox) findViewById(R.id.cBox3);
+
+        final EditText editQues = (EditText) findViewById(R.id.edit_text);
 
         Button submit = (Button) findViewById(R.id.btnSubmit);
         if (radioGroup != null) {
@@ -96,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+
         if (submit != null) {
             submit.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -109,7 +121,15 @@ public class MainActivity extends AppCompatActivity {
                         score++;
                     if (answerFour == 1)
                         score++;
-                    Toast.makeText(MainActivity.this, "Your score is " + score + " out of 4", Toast.LENGTH_LONG).show();
+                    if (checkBox2 != null && checkBox1 != null && checkBox != null && checkBox.isChecked() && checkBox1.isChecked() && !checkBox2.isChecked())
+                        score++;
+                    if (editQues.length() != 0)
+                        if (editQues.getText().toString().equals("Alphabet")) {
+                            editQues.setText("");
+                            score++;
+                        } else
+                            score--;
+                    Toast.makeText(MainActivity.this, "Your score is " + score + " out of 6", Toast.LENGTH_LONG).show();
                 }
             });
         }
